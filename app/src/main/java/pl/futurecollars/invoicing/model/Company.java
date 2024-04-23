6e043eb685237d1d253aa473afe8e07201943e8f
Arek.Urbanski <arek.urbanski@gmail.com>
2024-04-23 19:04:17 +0200

    Lesson 9 - only application without working tests

--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,11 +1,15 @@
 package pl.futurecollars.invoicing.model;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Company {
 
   @ApiModelProperty(value = "Tax identification number", required = true, example = "123-123-11-11")
@@ -14,10 +18,4 @@ public class Company {
   private String address;
   @ApiModelProperty(value = "Company name", required = true, example = "Nad nasza wsia przelecial meteoryt S.A.")
   private String name;
-
-  public Company(String taxIdentificationNumber, String address, String name) {
-    this.taxIdentificationNumber = taxIdentificationNumber;
-    this.address = address;
-    this.name = name;
-  }
 }
