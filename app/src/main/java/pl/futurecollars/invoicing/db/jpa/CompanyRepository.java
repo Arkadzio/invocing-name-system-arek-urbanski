be6764dc82e820a77b522e1c5fdd9ab4a2cb028f
Arek.Urbanski <arek.urbanski@gmail.com>
2024-05-21 11:46:46 +0200

    Lesson 15

--- app/src/main/java/pl/futurecollars/invoicing/db/jpa/CompanyRepository.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/jpa/CompanyRepository.java
@@ -1,6 +1,5 @@
 package pl.futurecollars.invoicing.db.jpa;
 
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.futurecollars.invoicing.model.Company;
